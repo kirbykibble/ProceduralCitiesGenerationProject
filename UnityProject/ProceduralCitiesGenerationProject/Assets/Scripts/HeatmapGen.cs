4e5bd57705f39056ca8256150429d93997af1a51
dylan chew <maxmanmii@gmail.com>
2020-01-14 23:48:02 -0800

    reuploaded. Face an upload error with last commit

---
+++ UnityProject/ProceduralCitiesGenerationProject/Assets/Scripts/HeatmapGen.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.IO;
+
+public class HeatmapGen : MonoBehaviour
+{
+    public int size;
+    public float randomness; //set between 0 and 1
+    public float randAmount; //set between 0 and 1 is maximum randomness applied.
+    public int center;
+
+    private Texture2D heatmap;
+    private Color color = Color.black;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Texture2D heatmap = new Texture2D(size, size);
+        center = (size - (size % 2)) / 2;
+        //heatmap.SetPixel(0, 0, Color.white);
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                color.g = 0;
+                float iClose = (Mathf.Abs(i - center));
+                iClose = iClose / center;
+                iClose = 1 - iClose;
+
+                float jClose = (Mathf.Abs(j - center));
+                jClose = jClose / center;
+                jClose = 1 - jClose;
+
+                float output = iClose + jClose;
+                output = output / 2;
+
+                if (Random.value < randomness)
+                {
+                    int upOrDown = Random.Range(0, 2);
+                    float amtToChange = Random.Range(0, randAmount * 100);
+                    amtToChange = amtToChange / 100;
+                    if(upOrDown == 0)
+                    {
+                        //shifts the heatmap up
+                        output += amtToChange;
+                    }
+                    if(upOrDown == 1)
+                    {
+                        //shifts the heatmap down
+                        output -= amtToChange;
+                    }
+                    //color.g = 1; //shows which pixels were affected. Use for debug purposes only.
+                }
+
+                if (output > 1)
+                {
+                    output = 1;
+                }
+                if (output < 0)
+                {
+                    output = 0;
+                }
+                color.r = output;
+                heatmap.SetPixel(i, j, color);
+            }
+        }
+
+
+
+        //outputs the created heatmap to a png. For debugging purposes only. 
+        byte[] bytes = heatmap.EncodeToPNG();
+        Object.Destroy(heatmap);
+        File.WriteAllBytes(Application.dataPath + "/../Assets/Debug/HeatmapOut.png", bytes);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UnityProject/ProceduralCitiesGenerationProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRigid;
+    public float speed = 5;
+
+    public float mouseSensitivity = 100.0f;
+    public float clampAngle = 80.0f;
+
+    private float rotY = 0.0f; // rotation around the up/y axis
+    private float rotX = 0.0f; // rotation around the right/x axis
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigid = this.GetComponent<Rigidbody>();
+
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            playerRigid.transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerRigid.transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerRigid.transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerRigid.transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
+
+        rotY += mouseX * mouseSensitivity * Time.deltaTime;
+        rotX += mouseY * mouseSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+    }
+}
